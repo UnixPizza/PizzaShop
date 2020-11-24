dcb843b126be1f6c11060f2c2540a10c4a16b60f
hyun99999 <ypsophila97@icloud.com>
2020-11-24 13:32:43 +0900

    file split init

--- PizzaShop/pizzashop.c
+++ PizzaShop/pizzashop.c
@@ -1,14 +1,2 @@
-typedef struct _Table {
-	int ID;
+#include "pizzashop.h"
 
-	int Combination;
-	int Potato;
-	int Pepperoni;
-	int Cheese;
-	int Tomato;
-	int Cream;
-	int Coke;
-	int Cider;
-
-	int TotalSales;
-}Table;
--- PizzaShop/pizzashop.h
+++ PizzaShop/pizzashop.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <stdio.h>
+
+typedef struct _Table {
+	int ID;
+
+	int Combination;
+	int Potato;
+	int Pepperoni;
+	int Cheese;
+	int Tomato;
+	int Cream;
+	int Coke;
+	int Cider;
+
+	int TotalSales;
+}Table;
